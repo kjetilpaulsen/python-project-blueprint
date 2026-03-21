99aa56b6a867cc79416f57b1fbc73b13cf26b196
Kjetil Paulsen <kjetil_paulsen@hotmail.com>
2026-03-21 19:29:46 +0100

    created registry in clieventhandler instead of if statements

--- src/python_project_blueprint/cli/clieventhandler.py
+++ src/python_project_blueprint/cli/clieventhandler.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
 import logging
+from typing import Callable, Iterator
+from typing_extensions import Any
 
 # FIX: change project name for imports
 from python_project_blueprint.commands.commands import CmdDisplayVersion, Command
@@ -28,11 +30,19 @@ class CliEventHandler:
         - `EvtResult`: command execution results
 
     Unrecognized event types are logged as warnings.
+    FIX: update docstring
     """
     def __init__(self) -> None:
-        pass
+        self._events: dict[type[Event], Callable] ={
+            EvtLog: lambda evt: self._handle_evtlog(evt),
+            EvtProgress: lambda evt: self._handle_evtprogress(evt),
+            EvtError: lambda evt: self._handle_evterror(evt),
+            EvtResult: lambda evt: self._handle_evtresult(evt),
+            EvtRequest: lambda evt: self._handle_evtrequest(evt),
 
-    def handle(self, evt: Event) -> None | Command:
+        }
+
+    def handle(self, evt: Event) -> Command | None:
         """
         Process an application event and convert it into CLI output.
 
@@ -46,25 +56,42 @@ class CliEventHandler:
 
         Returns:
             None
+        FIX: update docstring
         """
-        if isinstance(evt, EvtLog):
-            logger.info("%s", evt.message)
-            return None
-        elif isinstance(evt, EvtProgress):
-            # Build progressbar
-            return None
-        elif isinstance(evt, EvtError):
-            logger.error("%s - Fatal=%s", evt.message, evt.fatal)
-            return None
-        elif isinstance(evt, EvtResult):
-            logger.info("Handling EvtResult ..")
-            print(f"{evt.command_name} - {evt.payload}")
-            return None
-        elif isinstance(evt, EvtRequest):
-            logger.info("Requesting from user")
-            # call request function, get returned frontendinputcommand, 
-            # send frontendinputcommand to build_command(), return the built command
-            return None
-        else:
-            logger.warning("Unhandled event type: %s", type(evt).__name__)
-            return None
+        event = self._events.get(type(evt))
+        if event is None:
+            logger.critical(f"Event not found in _events: {type(evt).__name__}")
+        return event(evt)
+        # if isinstance(evt, EvtLog):
+        #     logger.info("%s", evt.message)
+        #     return None
+        # elif isinstance(evt, EvtProgress):
+        #     # Build progressbar
+        #     return None
+        # elif isinstance(evt, EvtError):
+        #     logger.error("%s - Fatal=%s", evt.message, evt.fatal)
+        #     return None
+        # elif isinstance(evt, EvtResult):
+        #     logger.info("Handling EvtResult ..")
+        #     print(f"{evt.command_name} - {evt.payload}")
+        #     return None
+        # elif isinstance(evt, EvtRequest):
+        #     logger.info("Requesting from user")
+        #     # call request function, get returned frontendinputcommand, 
+        #     # send frontendinputcommand to build_command(), return the built command
+        #     return None
+        # else:
+        #     logger.warning("Unhandled event type: %s", type(evt).__name__)
+        #     return None
+
+    def _handle_evtlog(self, evt) -> None:
+        return None
+    def _handle_evtprogress(self, evt) -> None:
+        return None
+    def _handle_evterror(self, evt) -> None:
+        return None
+    def _handle_evtresult(self, evt) -> None:
+        logger.info(f"{evt.command_name}: {evt.payload}")
+        return None
+    def _handle_evtrequest(self, evt) -> None:
+        return None
